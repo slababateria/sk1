56651afe510ed38572c0fe7cea35c86e86196daa
slababateria <slababateria@gmail.com>
2018-03-28 23:54:01 +0200

    wyrzucone to, co jednak nie działa

--- daemon.sh
+++ daemon.sh
@@ -3,30 +3,24 @@
 PIDFILE=serverpid.pid
 COMMAND=$1
 PID=$(cat $PIDFILE)
-TEST=$(ps | grep $PID)
 
 #start
 if test $COMMAND = 'start'
 then
-   if $TEST
-   then
-      echo BŁĄD: Proces już jest uruchomiony.
-   else
-      ./server.sh &
-   fi
+    ./server.sh &
 #stop
 elif test $COMMAND = 'stop'
 then
-   kill $PID
+    kill $PID
 #reset
 elif test $COMMAND = 'reset'
 then
-   kill $PID
-   ./server.sh
+    kill $PID
+    ./server.sh &
 #status
 elif test $COMMAND = 'status'
 then
-   echo test
+    echo test
 else
-   echo Nierozpoznana komenda.
+    echo Nierozpoznana komenda.
 fi

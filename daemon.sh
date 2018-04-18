590a0f6a9b887a93f1f62f605dc5addcd22b10d4
slababateria <slababateria@gmail.com>
2018-04-18 22:14:21 +0200

    Add files via upload

--- daemon.sh
+++ daemon.sh
@@ -8,43 +8,70 @@ if test $COMMAND = 'start'
 then
     if test -e $PIDFILE
     then
-        echo "BŁĄÐ: Program już jest uruchomiony!"
+        PID=$(cat $PIDFILE)
+        if test -d /proc/$PID
+        then
+            echo "BŁĄÐ: Program już jest uruchomiony!"
+        else
+            ./server.sh &
+            echo $! > $PIDFILE
+        fi
     else
         ./server.sh &
         echo $! > $PIDFILE
     fi
+
 #stop
 elif test $COMMAND = 'stop'
 then
     if test -e $PIDFILE
     then
         PID=$(cat $PIDFILE)
-        kill $PID
-        rm $PIDFILE
+        if test -d /proc/$PID
+        then
+            kill $PID
+            rm $PIDFILE
+        else
+           echo "BŁĄÐ: Program nie był uruchomiony!"
+        fi
     else
-        echo "BŁĄÐ: Program nie jest uruchomiony!"
+        echo "BŁĄÐ: Brak pidfile!"
     fi
+    
 #reset
 elif test $COMMAND = 'reset'
 then
     if test -e $PIDFILE
     then
         PID=$(cat $PIDFILE)
-        kill $PID
-        ./server.sh &
-        echo $! > $PIDFILE
+        if test -d /proc/$PID
+        then
+            kill $PID
+            ./server.sh &
+            echo $! > $PIDFILE
+        else
+            ./server.sh &
+            echo $! > $PIDFILE
+        fi
     else
-        echo "BŁĄÐ: Program nie jest uruchomiony!"
+        echo "BŁĄÐ: Brak pidfile!"
     fi
+    
 #status
 elif test $COMMAND = 'status'
 then
     if test -e $PIDFILE
     then
-        echo "Program jest uruchomiony. PID:" $(cat $PIDFILE)
+        PID=$(cat $PIDFILE)
+        if test -d /proc/$PID
+            then
+            echo "Program jest uruchomiony. PID:" $PID
+        else
+            echo "Program nie jest uruchomiony."
+        fi
     else
-        echo "Program nie jest uruchomiony."
+        echo "BŁĄÐ: Brak pidfile!"
     fi
 else
-    echo Nierozpoznana komenda.
+    echo "BŁĄÐ:Nierozpoznana komenda!"
 fi
